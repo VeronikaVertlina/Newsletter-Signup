cb4a11c4781158eeaa7af39df738d4e91f5376b5
ronya78@gmail.com <ronya78@gmail.com>
2022-01-27 13:12:47 +0200

    Add code

--- app.js
+++ app.js
@@ -2,15 +2,24 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
-
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  res.send("Server is up and running.");
+  res.sendFile(__dirname + "/signup.html");
+  // res.send("Server is up and running.");
 });
 
+app.post("/", function(req, res){
+  var firstName = req.body.fName;
+  var lastName = req.body.lName;
+  var email = req.body.email;
+  res.send(firstName + " " + lastName + " " + email);
+});
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
