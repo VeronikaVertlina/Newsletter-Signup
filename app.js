4113117ba66ab07c6a8cf70a081e5eda8dda3d96
ronya78@gmail.com <ronya78@gmail.com>
2022-01-31 14:17:49 +0200

    Add code

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
@@ -15,11 +16,50 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-  var firstName = req.body.fName;
-  var lastName = req.body.lName;
-  var email = req.body.email;
-  res.send(firstName + " " + lastName + " " + email);
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  // res.send(firstName + " " + lastName + " " + email);
+  //new variable called data going to set that as a new Javascript object
+  //inside our data object, we have to provide all the key value pairs
+  const data = {
+    memebers: [
+      email_address: email,
+      status: "subscribed",
+      merge_fiels: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    ]
+  };
+
+  // I'm going to pass in my data in JSON.stringify()
+  const jsonData = JSON.stringify(data);
+
+  //going to pass url,options,callback function, which is going to give us
+  //a respounse from the MailChimp server.
+  //"https://$API_SERVER.api.mailchimp.com/3.0/lists"
+  const url = "https://us14.api.mailchimp.com/3.0/lists/44be01369a" ;
+
+  //Create some options and it's going to be a Javascript oblect
+    const options = {
+    method: "POST",
+    auth: "VeronikaVertlina: 1a4b130f476c5cafe8a19d871bb670a1-us14"
+  }
+  https.request(url, options, function(){
+
+
+  });
 });
+
+{
+  "name": "$event_name",
+  "contact": $footer_contact_info,
+  "permission_reminder": "permission_reminder",
+  "email_type_option": true,
+  "campaign_defaults": $campaign_defaults
+}
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
