5fadb506e531f534b5281cb05fa581bae21f6b2a
ronya78@gmail.com <ronya78@gmail.com>
2022-01-31 16:32:29 +0200

    Add code

--- app.js
+++ app.js
@@ -2,13 +2,18 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mailchimp = require("mailchimp/mailchimp_marketing");
 const request = require("request");
 const https = require("https");
 
 const app = express();
-
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+mailchimp.setConfig({
+  apiKey: "1a4b130f476c5cafe8a19d871bb670a1",
+  server: "us14"
+});
 
 app.get("/", function(req, res){
   res.sendFile(__dirname + "/signup.html");
@@ -16,50 +21,60 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
-  const firstName = req.body.fName;
-  const lastName = req.body.lName;
-  const email = req.body.email;
+
+  const listId = "44be01369a";
+  const subscribingUser = {
+    firstName: req.body.fName,
+    lastName: req.body.lName,
+    email: req.body.email
+  };
 
   // res.send(firstName + " " + lastName + " " + email);
   //new variable called data going to set that as a new Javascript object
   //inside our data object, we have to provide all the key value pairs
-  const data = {
-    memebers: [
-      email_address: email,
-      status: "subscribed",
-      merge_fiels: {
-        FNAME: firstName,
-        LNAME: lastName
-      }
-    ]
-  };
-
-  // I'm going to pass in my data in JSON.stringify()
-  const jsonData = JSON.stringify(data);
-
-  //going to pass url,options,callback function, which is going to give us
-  //a respounse from the MailChimp server.
-  //"https://$API_SERVER.api.mailchimp.com/3.0/lists"
-  const url = "https://us14.api.mailchimp.com/3.0/lists/44be01369a" ;
-
-  //Create some options and it's going to be a Javascript oblect
-    const options = {
-    method: "POST",
-    auth: "VeronikaVertlina: 1a4b130f476c5cafe8a19d871bb670a1-us14"
-  }
-  https.request(url, options, function(){
-
-
+async function run() {
+  const response = await mailchimp.lists.addListMember(listId, {
+    email_address: subscribingUser.email,
+    status: "subscribed",
+    merge_fields: {
+      FNAME: subscribingUser.firstName,
+      LNAME: subscribingUser.lastName
+    }
   });
+
+  console.log(
+    "Succeddfuly added contact as an audience member. The contact's id is " + response.id + " ."
+  );
+}
+
+run();
 });
 
-{
-  "name": "$event_name",
-  "contact": $footer_contact_info,
-  "permission_reminder": "permission_reminder",
-  "email_type_option": true,
-  "campaign_defaults": $campaign_defaults
-}
+  // // I'm going to pass in my data in JSON.stringify()
+  // const jsonData = JSON.stringify(data);
+  //
+  // //going to pass url,options,callback function, which is going to give us
+  // //a respounse from the MailChimp server.
+  // //"https://$API_SERVER.api.mailchimp.com/3.0/lists"
+  // const url = "https://us14.api.mailchimp.com/3.0/lists/44be01369a" ;
+  //
+  // //Create some options and it's going to be a Javascript oblect
+  // const options = {
+  //   method: "POST",
+  //   auth: "Veronika Vertlina:1a4b130f476c5cafe8a19d871bb670a1-us14"
+  // }
+  // const request = https.request(url, options, function(response){
+  //   response.on("data", function(data){
+  //     console.log(JSON.parse(data));
+  //   });
+  // });
+  //
+  // request.write(jsonData);
+  // request.end();
+// });
+
+
+
 
 app.listen(3000, function(){
   console.log("Server is running on port 3000.");
